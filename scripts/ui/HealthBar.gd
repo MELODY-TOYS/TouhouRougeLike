4eff6d8b06b61eaf8830dd5d1794682d8de1845b
MELODY-TOYS <tongfuUE@gmail.com>
2025-09-23 12:44:56 +0900

    收尾enemy模块

--- scripts/ui/HealthBar.gd
+++ scripts/ui/HealthBar.gd
@@ -8,24 +8,24 @@ func _ready() -> void:
 	# 默认隐藏，在找到有效目标前，不应该显示。
 	visible = false
 
-	# --- "即插即用"的核心逻辑 ---
-	# 作为一个“组件”，它的首要任务是服务于它的直接父节点。
-	var owner = get_parent()
-	
-	# 安全检查：确保父节点存在。
-	if not is_instance_valid(owner):
-		push_warning("HealthBar: 必须被附加到一个父节点下才能自动工作。")
-		return
-
-	# 使用ASC服务，从父节点身上查找StatsComponent。
-	var stats_component = ASC.get_stats_component_from(owner)
-
-	# 如果成功找到了，就自动激活自己。
-	if is_instance_valid(stats_component):
-		track_stats(stats_component)
-	else:
-		# 这是一个有用的警告：你把血条放到了一个没有属性的物体上。
-		push_warning("HealthBar: 在父节点 '%s' 上未能找到StatsComponent。" % owner.name)
+	## --- "即插即用"的核心逻辑 ---
+	## 作为一个“组件”，它的首要任务是服务于它的直接父节点。
+	#var owner = get_parent()
+	#
+	## 安全检查：确保父节点存在。
+	#if not is_instance_valid(owner):
+		#push_warning("HealthBar: 必须被附加到一个父节点下才能自动工作。")
+		#return
+#
+	## 使用ASC服务，从父节点身上查找StatsComponent。
+	#var stats_component = ASC.get_stats_component_from(owner)
+#
+	## 如果成功找到了，就自动激活自己。
+	#if is_instance_valid(stats_component):
+		#track_stats(stats_component)
+	#else:
+		## 这是一个有用的警告：你把血条放到了一个没有属性的物体上。
+		#push_warning("HealthBar: 在父节点 '%s' 上未能找到StatsComponent。" % owner.name)
 
 
 # -------------------- 公共接口 (依然保留) --------------------
